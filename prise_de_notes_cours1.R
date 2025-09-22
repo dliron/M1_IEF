fdd8b68e8d3bee3630bea528d68c49f9dbc4ba13
dliron <73888682+dliron@users.noreply.github.com>
2025-09-22 10:02:38 +0200

    Add files via upload

--- prise_de_notes_cours1.R
+++ prise_de_notes_cours1.R
@@ -96,3 +96,152 @@ monvec = c(monvec, 2)
 
 monvec[3] + monvec[2]
 length(monvec)
+monvec[length(monvec)]
+
+
+# Les deux premiers termes de la suite de Fibonacci sont tous deux un. 
+# Les termes suivants de la séquence sont trouvés en additionnant les deux termes immédiatement précédents. 
+# Écrivez un code qui écrit les n termes de la séquence de Fibonacci pour tout n≥3
+nb_fibo = 100
+fibo = c(1, 1)
+for( i in 1:nb_fibo){
+  new_fibo = fibo[length(fibo)] + fibo[length(fibo)-1]
+  fibo = c(fibo, new_fibo)
+  # fibo[length(fibo)+1] = new_fibo
+}
+fibo
+
+# Écrivez un code qui simule le lancer d'une pièce avec une 
+# probabilité p associée aux piles,  jusqu’à obtenir fâce, 
+# et renvoie le nombre de piles obtenues. Ensuite, reproduisez 
+# l’exécution de ce code 100 fois et afficher le nombre maximum 
+# de piles obtenus lors d’une même séquence. Pour générer une
+# probabilité entre 0 et 1, utilisez runif(1).
+max_sur_100try = 0
+for( i in 1:100){
+  max_try = 1
+  
+  resultat = runif(1)
+  while(resultat < 0.5){
+    resultat = runif(1)
+    max_try = max_try + 1
+  }
+  
+  if(max_try > max_sur_100try){
+    max_sur_100try = max_try
+  }
+  
+}
+print(max_sur_100try)
+
+
+myfunction <- function(a, b="valeur de v", c = "valeur de c") {
+  print(a)
+  print(b)
+  print(c)
+}
+
+
+somme2val <- function(a, b) {
+  res = a + b
+  return(res)
+}
+
+
+
+
+func_estpremier <- function(myval) {
+  estpremier = TRUE
+  for( i in 2:(myval-1)) {
+    if( a %% i == 0){
+      print(sprintf("%s n'est pas un nombre premier car il est divisible pas %s", a, i))
+      estpremier = FALSE
+      break
+    }
+  }
+  
+  if( estpremier ){
+    print("Le nombre est premier")
+  } else {
+    print("Il est pas premier")
+  }
+  
+  return(estpremier)
+}
+
+res_estpremier = func_estpremier(7)
+
+
+
+
+func_fibo <- function(nb_items) {
+  fibo = c(1, 1)
+  for( i in 1:nb_items){
+    new_fibo = fibo[length(fibo)] + fibo[length(fibo)-1]
+    fibo = c(fibo, new_fibo)
+  }
+  return(fibo)
+}
+res_fibo = func_fibo(nb_items=10)
+
+
+func_pileface <- function(probap = 0.5, nb_iter = 100) {
+  max_sur_100try = 0
+  for( i in 1:nb_iter){
+    max_try = 1
+    
+    resultat = runif(1)
+    while(resultat < probap){
+      resultat = runif(1)
+      max_try = max_try + 1
+    }
+    
+    if(max_try > max_sur_100try){
+      max_sur_100try = max_try
+    }
+    
+  }
+  return(max_sur_100try)
+}
+
+func_pileface()
+func_pileface(probap = 0.04)
+func_pileface(nb_iter=10)
+func_pileface(probap = 0.99, nb_iter=1000)
+
+
+
+
+taille  = c(121, 156, NA, 234)
+poids    = c(67, 78, 235, 567)
+sexe    = c("Homme","Femme","Homme", "homme")
+
+mydata = data.frame(taille = taille, poids = poids, sexe = sexe)
+class(mydata)
+
+colnames(mydata)
+rownames(mydata)
+dim(mydata)
+nrow(mydata)
+ncol(mydata)
+summary(mydata)
+
+
+mydata[ , ]
+mydata[ 2, ]
+mydata[ , 3]
+mydata[ 1, 3]
+mydata[ c(1,4), ]
+
+mydata[ , "poids"]
+mydata[ , c("poids","taille")]
+mydata["poids"]
+mydata$poids
+
+mydata[ mydata$poids >= 100, ]
+mydata[ mydata$poids >= 100, "poids"]
+
+
+
+
+
