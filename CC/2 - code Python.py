c855e208cea0babaa7c49c4212f82b33e1b24275
dliron <73888682+dliron@users.noreply.github.com>
2025-10-13 15:54:14 +0200

    Add files via upload

---
+++ CC/2 - code Python.py
@@ -0,0 +1,46 @@
+#########################################################################
+#########               Seconde partie : Python (3 pts)
+#########################################################################
+
+# 1 - importer le fichier baseR.csv du repertoire /output (0.5pts)
+# Si vous n'avez pas réussis à le faire dans la partie R, partir du fichier baseR_help.csv
+
+
+# 2 - Transformer ces données en série temporelle (0.5pts)
+
+
+# 3 - Renommer les colonnes brent en energyPrices, debt en netDebt et gdp en rgdp (1 pts)
+
+
+# 4 - Calculer la moyenne de chaque colonne (0.5pts)
+
+
+# 5 - Afficher le nombre de 0 et de 1 de la colonne dummy_debt (0.5pts)
+
+
+
+###########################################################################
+######### Troiseme partie : R ou Python (Choisir un seul des deux langages) (3.5pts)
+###########################################################################
+
+# Écrire un programme qui génère un nombre aléatoire entre 1 et 100 
+    # from random import *
+    # randint(0, 100)
+# Ensuite, demande à l'utilisateur de deviner ce nombre. 
+    # int(input("Saisissez un entier : "))
+# Le programme doit lui donner des indices (trop grand, trop petit) jusqu'à ce que l'utilisateur trouve le bon nombre. 
+# Affiche le nombre de tentatives qu'il a fallu pour deviner correctement.
+
+# Faire une fonction avec en parametre le range de valeur à deviner (ici entre 0 et 100)
+# Et retourner le nombre de tentatives
+
+
+
+
+
+
+
+
+
+
+
